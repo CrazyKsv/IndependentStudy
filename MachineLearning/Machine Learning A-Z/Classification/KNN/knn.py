c9a8fad3073d6a245f36bf1403da985b3d52d374
kskod <cyndi@cyndideMacBook-Pro.local>
2018-05-29 13:14:24 -0400

    K nearest Neighbour

--- MachineLearning/Machine Learning A-Z/Classification/KNN/knn.py
+++ MachineLearning/Machine Learning A-Z/Classification/KNN/knn.py
@@ -1,4 +1,4 @@
-# Logistic Regression
+# K-Nearest Neighbors (K-NN)
 
 # Importing the libraries
 import numpy as np
@@ -8,9 +8,11 @@ from sklearn.cross_validation import train_test_split
 
 from sklearn.preprocessing import StandardScaler
 
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 
+# Visualising the Training set results
 from matplotlib.colors import ListedColormap
+from sklearn.metrics import confusion_matrix
 FILE = 'Social_Network_Ads.csv'
 
 
@@ -19,7 +21,7 @@ def preprocess(file, x_start, x_end, y_ind):
     
     dataset = pd.read_csv(file)
     # 2,3
-    X = dataset.iloc[:, [x_start, x_end]].values
+    X = dataset.iloc[:, x_start:x_end].values
     # 4
     y = dataset.iloc[:, y_ind].values
 
@@ -38,7 +40,8 @@ def featureScaling(X_train, X_test):
     return X_train, X_test, sc
 
 
-def logisticRegression_train(classifier, X_train, y_train):
+def KNN_classifier_train(classifier, X_train, y_train):
+    # Fitting K-NN to the Training set
     classifier.fit(X_train, y_train)
     X_set, y_set = X_train, y_train
     X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -50,15 +53,15 @@ def logisticRegression_train(classifier, X_train, y_train):
     for i, j in enumerate(np.unique(y_set)):
         plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                     c = ListedColormap(('red', 'green'))(i), label = j)
-    plt.title('Logistic Regression (Training set)')
+    plt.title('K-NN (Training set)')
     plt.xlabel('Age')
     plt.ylabel('Estimated Salary')
     plt.legend()
     plt.show()
     return classifier
+    
 
-
-def logisticRegression_test(classifier, X_test, y_test):
+def KNN_classifier_test(classifier, X_test, y_test):
     X_set, y_set = X_test, y_test
     X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                          np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
@@ -69,32 +72,31 @@ def logisticRegression_test(classifier, X_test, y_test):
     for i, j in enumerate(np.unique(y_set)):
         plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                     c = ListedColormap(('red', 'green'))(i), label = j)
-    plt.title('Logistic Regression (Test set)')
+    plt.title('K-NN (Test set)')
     plt.xlabel('Age')
     plt.ylabel('Estimated Salary')
     plt.legend()
     plt.show()
-    
+
 
 def classify(classifier, age, sal, sc):
-    
     pre_value = sc.transform(np.array([[age, sal]]))
     result = classifier.predict(pre_value)
     return result
 
 
 if __name__ == '__main__':
-    X_train, X_test, y_train, y_test, X, y = preprocess(FILE, 2, 3, -1)
+    X_train, X_test, y_train, y_test, X, y = preprocess(FILE, 2, 4, -1)
     X_train, X_test, sc = featureScaling(X_train, X_test)
-    classifier = LogisticRegression(random_state = 0)
-    classifier = logisticRegression_train(classifier, X_train, y_train)
-    logisticRegression_test(classifier, X_test, y_test)
-    
-    y_predict = classifier.predict(X_test)
-    
-    print(classify(classifier, 30, 10000, sc))
-    
-    
-    #classifier.predict(np.array([[25,10000]]))
 
+
+    
+    classifier = KNeighborsClassifier(n_neighbors = 9, metric = 'minkowski', p = 2)
+    classifier = KNN_classifier_train(classifier, X_train, y_train)
+    KNN_classifier_test(classifier, X_test, y_test)
+    # Predicting the Test set results
+    y_predict = classifier.predict(X_test)
+    # Making the Confusion Matrix
+    cm = confusion_matrix(y_test, y_predict)
+    print(classify(classifier, 34, 150000, sc))
     
