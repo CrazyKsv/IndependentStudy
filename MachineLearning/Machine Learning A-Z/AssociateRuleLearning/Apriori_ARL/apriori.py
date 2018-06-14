fc27095fea079fcab01851350973310834c2f994
kskod <kodks.hu@gmail.com>
2018-06-14 11:21:08 -0400

    neural net 3 layer

--- MachineLearning/Machine Learning A-Z/AssociateRuleLearning/Apriori_ARL/apriori.py
+++ MachineLearning/Machine Learning A-Z/AssociateRuleLearning/Apriori_ARL/apriori.py
@@ -14,22 +14,20 @@ def apriori_learning(file):
     Given a data file, Learn from the data and output associate relation between items
     '''
     # read the data
-    dataset = pd.read_csv(file)
-
+        
+    dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
     transactions = []
-    for i in range(len(dataset[:,0])):
-        transactions.append(str(dataset.values[i,j] for j in range(len(dataset[0,:]))))
-    # Training Apriori on the dataset
+    for i in range(0, len(dataset)):
+        transactions.append([str(dataset.values[i,j]) for j in range(0, len(dataset.values[0,:]))])
+    
+    
     rules = apriori(transactions, min_support = 0.003,
                     min_confidence = 0.2, min_lift = 3, min_length = 2)
     rules = list(rules)
     return rules
 
-# dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
-# transactions = []
-# for i in range(0, 7501):
-#     transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
-
-
 # # Visualising the results
-# results = list(rules)
+if __name__ == '__main__':
+    rules = apriori_learning(FILE)
+    for i in range(len(rules)):
+        print(rules[i].items)
