762a5eeabcbafb5d04be7eb3853c7fb0ef476e75
CrazyKs <kodks.hu@gmail.com>
2021-04-24 15:03:09 +0800

    nested component

--- MERN/crazy/src/index.js
+++ MERN/crazy/src/index.js
@@ -1,8 +1,29 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greeting(){
-  return <h4>Life is intense</h4>;
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Age />
+    </div>
+  )
 }
 
-ReactDom.render(<Greeting/>, document.getElementById("root"));
+function Person() {
+  return (
+    <div>
+      <h1>Kevin Hu</h1>
+    </div>
+  )
+}
+
+function Age() {
+  return (
+    <div>
+      <h3>38</h3>
+    </div>
+  )
+}
+
+ReactDom.render(<Greeting />, document.getElementById('root'))
