ab11671fd4fdb28c491eb476c78929d8cc71dfd6
CrazyKs <kodks.hu@gmail.com>
2021-05-04 21:59:31 +0800

    form practise

--- MERN/advanceReact/src/App.js
+++ MERN/advanceReact/src/App.js
@@ -4,14 +4,16 @@ import Comp2 from './tutorial/1-useState/setup/4-useState-object'
 import Counter from './tutorial/1-useState/setup/5-useState-counter'
 import GitUser from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 import Merror from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+import Form from './tutorial/4-forms/setup/1-controlled-inputs'
 function App() {
   return (
     <div className='container'>
-      <Comp1 />
+      {/* <Comp1 />
       <Comp2 />
-      <Counter />
-      <GitUser />
-      <Merror />
+      <Counter /> */}
+      {/* <GitUser /> */}
+      {/* <Merror /> */}
+      <Form />
     </div>
   )
 }
--- MERN/advanceReact/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ MERN/advanceReact/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,75 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
-};
+  const [person, setPerson] = useState({ firstName: '', email: '', age: '' })
+  const [people, setPeople] = useState([])
 
-export default ControlledInputs;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (person.firstName && person.email && person.age) {
+      const newP = { id: new Date().getTime().toString(), ...person }
+      setPeople((people) => {
+        return [...people, newP]
+      })
+      setPerson({ firstName: '', email: '', age: '' })
+    }
+  }
+
+  const handleOnChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({ ...person, [name]: value })
+  }
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name: </label>
+            <input
+              type='text'
+              name='firstName'
+              id='firstName'
+              value={person.firstName}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email: </label>
+            <input
+              type='text'
+              name='email'
+              id='email'
+              value={person.email}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='age'>Age : </label>
+            <input
+              type='text'
+              name='age'
+              id='age'
+              value={person.age}
+              onChange={handleOnChange}
+            />
+          </div>
+          <button type='submit'> add person</button>
+        </form>
+        {people.map((p) => {
+          const { id, firstName, email, age } = p
+          return (
+            <div className='item' key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+              <p>{age}</p>
+            </div>
+          )
+        })}
+      </article>
+    </>
+  )
+}
+
+export default ControlledInputs
