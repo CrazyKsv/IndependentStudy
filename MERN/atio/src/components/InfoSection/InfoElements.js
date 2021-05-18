893cbadd93ab21e4f5055226f7c32e623ee72758
CrazyKs <kodks.hu@gmail.com>
2021-05-18 18:11:00 +0800

    image pos done

--- MERN/atio/src/components/InfoSection/InfoElements.js
+++ MERN/atio/src/components/InfoSection/InfoElements.js
@@ -41,6 +41,7 @@ export const Column1 = styled.div`
 `
 
 export const Column2 = styled.div`
+  margin-top: 0;
   margin-bottom: 15px;
   padding: 0 15px;
   grid-area: col2;
@@ -100,7 +101,29 @@ export const ImgWrap = styled.div`
   height: 100%;
 `
 
+export const SpecialImgWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  max-width: 555px;
+  justify-content: center;
+  align-items: center;
+  background-color: #f8f8f8;
+  height: 300px;
+  width: 400px;
+  left: 0px;
+  top: 421px;
+  border-radius: 0px;
+`
+
 export const Img = styled.img`
+  width: 100%;
+  margin-bottom: 0;
+  margin-right: 0;
+
+  padding-right: 0;
+`
+
+export const ImgSmall = styled.img`
   width: 42px;
   margin-bottom: 0;
   margin-right: 0;
@@ -124,4 +147,36 @@ export const ImgWorld = styled.img`
   margin-right: 0;
   margin-left: 5px;
   padding-right: 0;
+`
+
+export const WorkWrapper = styled.div`
+  display: grid;
+  z-index: 1;
+  height: 1563px;
+  width: 960px;
+  margin-right: auto;
+  margin-left: auto;
+  justify-content: start;
+`
+
+export const WorkRow = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr 1fr;
+  grid-template-areas: ${({ imgStart }) =>
+    imgStart ? "'col2 col1'" : "'col1 col2'"};
+  /* Must use '\' css class \'' */
+
+  @media screen and (max-width: 768px) {
+    grid-template-areas: ${({ imgStart }) =>
+      imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
+  }
+`
+
+export const InnerImg = styled.img`
+  height: 249px;
+  width: 87px;
+  left: 100px;
+  top: 446px;
+  border-radius: 0px;
+  margin: 30px;
 `
--- MERN/atio/src/components/InfoSection/index.js
+++ MERN/atio/src/components/InfoSection/index.js
@@ -12,11 +12,22 @@ import {
   SubDesWrap,
   ImgWorld,
   Img,
+  ImgSmall,
+  ImgWrap,
   WorkFooter,
+  WorkWrapper,
+  WorkRow,
+  SpecialImgWrap,
+  InnerImg,
 } from './InfoElements'
 
 import ImageBubble from '../../images/bubbleTea.svg'
 import ImageWorldMap from '../../images/worldMap.svg'
+import ImageFaceBook from '../../images/facebook.svg'
+import ImageArchive from '../../images/archive.svg'
+import ImageMS1 from '../../images/ms1.svg'
+import ImageMS from '../../images/ms.svg'
+import ImageMS2 from '../../images/ms2.svg'
 
 const InfoSection = ({
   lightBg,
@@ -40,7 +51,7 @@ const InfoSection = ({
               <TextWrapper>
                 <Heading lightText={lightText}>
                   {headline}
-                  <Img src={ImageBubble} alt='log' />
+                  <ImgSmall src={ImageBubble} alt='log' />
                 </Heading>
                 <Subtitle darkText={darkText}>{description}</Subtitle>
                 <SubDesWrap>{subdescription}</SubDesWrap>
@@ -52,6 +63,47 @@ const InfoSection = ({
             </Column1>
           </InfoRow>
         </InfoWrapper>
+
+        {/* Work details */}
+        <WorkWrapper>
+          <WorkRow imgStart={true}>
+            <Column2>
+              <ImgWrap>
+                <Img src={ImageFaceBook} alt='fb' />
+              </ImgWrap>
+            </Column2>
+            <Column1>test</Column1>
+          </WorkRow>
+
+          <WorkRow imgStart={true}>
+            <Column2>
+              <SpecialImgWrap>
+                <InnerImg src={ImageMS} alt='ms' />
+                <InnerImg src={ImageMS1} alt='ms' />
+              </SpecialImgWrap>
+            </Column2>
+            <Column1>sdfs</Column1>
+          </WorkRow>
+
+          <WorkRow imgStart={true}>
+            <Column2>
+              <SpecialImgWrap>
+                <InnerImg src={ImageMS} alt='ms-sec' />
+                <InnerImg src={ImageMS2} alt='ms' />
+              </SpecialImgWrap>
+            </Column2>
+            <Column1>test</Column1>
+          </WorkRow>
+
+          <WorkRow imgStart={true}>
+            <Column2>
+              <ImgWrap>
+                <Img src={ImageArchive} alt='archive' />
+              </ImgWrap>
+            </Column2>
+            <Column1>test</Column1>
+          </WorkRow>
+        </WorkWrapper>
       </InfoContainer>
     </>
   )
